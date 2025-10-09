b3d3a0a80c355a7b782a1807a1f6f7c8f237fe5c
samiat-nexus <contact.samiatnexus@gmail.com>
2025-10-09 17:59:31 +0600

    socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5001", {
+const socket = io(" https://si-communication-app.onrender.com", {
   transports: ["websocket"],
 });
 
